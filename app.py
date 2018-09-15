7883533f444c54c0a8e9bb3ef3c90bb0a4fe21b2
snorlak1999 <39644873+snorlak1999@users.noreply.github.com>
2018-09-16 06:06:53 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -5,6 +5,11 @@ import json
 import os
 import math
 
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support.select import Select
+from bs4 import BeautifulSoup
 
 from flask import Flask
 from flask import request
@@ -32,34 +37,75 @@ def webhook():
 
 
 def makeWebhookResult(req):  
-     if req.get("result").get("action") == "halo":
-        hasil = req.get("result").get("resolvedQuery")
+     if req.get("result").get("action") == "cek":
+        driver = webdriver.Chrome('./chromedriver.exe')
+        driver.get('https://akademik.ithb.ac.id/default.php?mod=roster%20ruangan')
+        #driver.find_element_by_link_text("Absensi Kuliah").click();
+        #driver.find_element_by_id("txtUsername").send_keys("1")
+        select = Select(driver.find_element_by_name("tgl"))
+        select.select_by_value(tgl)
+        select = Select(driver.find_element_by_name("bln"))
+        select.select_by_value(bln)
+        select = Select(driver.find_element_by_name("thn"))
+        select.select_by_value(thn)
+        select = Select(driver.find_element_by_name("lantai"))
+        select.select_by_value(lantai)
+        driver.find_element_by_name("cmd").click()
+
+
+        soup = BeautifulSoup(driver.page_source,'html.parser')
+        x = soup.find_all("tbody")
+        tempat = str(x[9]).split("<tr>")[4]
+        w = tempat.split("<tbody>")[1]
+        #tempat
+        r=[]
+        i=2
+        while i<27:
+            r.append(w.split(">")[i].split("<")[0])
+            i=i+2
+
+
+        jadwal =[]
+
+
+        #7.00 - 7.30
+        loop=5
+        while loop<31:
+            p1 = str(x[9]).split("<tr>")[loop]
+            jam = p1.split("</td>")[0].split('"')[12].split("<")[0].split(">")[1]
+            i=0
+            while i<13:
+                try:
+                    row = p1.split("</td>")[i].split('"')
+                    if (len(row)>3):
+                        if row[3]=="#FFCC66":
+                            SKS = int(row[7])/2
+                            ruangan = r[i]
+                            kode = row[10].split("<")[0].split(">")[1]
+                            if len(row)>12:
+                                dosen = row[12].split("<")[0].split(">")[1]
+                                jadwal.append(kode+"\n"+dosen+" ("+str(SKS)+" SKS"+")\n"+ruangan+"\n"+jam)
+                            else:
+                                jadwal.append(kode+"\n"+" ("+str(SKS)+" SKS"+")\n"+ruangan+"\n"+jam)
+                    i=i+1
+                except Exception as res:
+                    i=i+1
+            loop=loop+1
+
+        hasil=""
+        loop=0
+        while loop<len(jadwal):
+            hasil = hasil + jadwal[loop] + "\n\n"
+            loop = loop+1
+            
         return {
-            "speech": hasil,
-            "displayText": hasil,
-            #"data": {},
-            #"contextOut": [],
-            "source": hasil
-        }
-     if req.get("result").get("action") == "penjumlahan":
-        bilangan1 = int(req.get("result").get("parameters").get("number"))
-        bilangan2 = int(req.get("result").get("parameters").get("number1"))
-        operasi = req.get("result").get("parameters").get("operasi")
-        if operasi == "+":
-            hasil = bilangan1+bilangan2
-        if operasi == "*":
-            hasil = bilangan1*bilangan2
-        if operasi == "/":
-            hasil = bilangan1/bilangan2
-        if operasi == "-":
-            hasil = bilangan1-bilangan2
-        return {
-            "speech": "hasil: "+str(hasil),
-            "displayText": "hasil: "+str(hasil),
-            #"data": {},
-            #"contextOut": [],
-            "source": "hasil: "+str(hasil)
-        }
+                "speech": hasil,
+                "displayText": hasil,
+                #"data": {},
+                #"contextOut": [],
+                "source": hasil
+            }
+
                     
     
     
