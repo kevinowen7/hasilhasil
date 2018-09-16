856c3887c50eeb5b99e437977c708870b0f929b8
snorlak1999 <39644873+snorlak1999@users.noreply.github.com>
2018-09-16 08:02:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -42,6 +42,7 @@ def makeWebhookResult(req):
         chrome_options.binary_location = "/app/.apt/usr/bin/google-chrome"
         chrome_options.add_argument('--disable-gpu')
         chrome_options.add_argument('--no-sandbox')
+        driver = webdriver.Chrome(executable_path="/app/.chromedriver/bin/chromedriver", chrome_options=chrome_options)
         return {
             "speech": "hasi",
             "displayText": "hasi",
@@ -49,7 +50,6 @@ def makeWebhookResult(req):
             #"contextOut": [],
             "source": "hasi"
         }
-        driver = webdriver.Chrome(executable_path="/app/.chromedriver/bin/chromedriver", chrome_options=chrome_options)
         driver.get('https://akademik.ithb.ac.id/default.php?mod=roster%20ruangan')
         #driver.find_element_by_link_text("Absensi Kuliah").click();
         #driver.find_element_by_id("txtUsername").send_keys("1")
