e1057f168b1ed469021005000448f1becceffee8
snorlak1999 <39644873+snorlak1999@users.noreply.github.com>
2018-09-16 21:40:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -57,6 +57,13 @@ def makeWebhookResult(req):
                 #"contextOut": [],
                 "source": e
             }
+        return {
+            "speech": "a",
+            "displayText": "a",
+            #"data": {},
+            #"contextOut": [],
+            "source": "a"
+        }
         driver.get('https://akademik.ithb.ac.id/default.php?mod=roster%20ruangan')
         #driver.find_element_by_link_text("Absensi Kuliah").click();
         #driver.find_element_by_id("txtUsername").send_keys("1")
