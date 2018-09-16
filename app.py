c5d51372c06b2d9fa6f2a06bedf52e35ed9335e4
snorlak1999 <39644873+snorlak1999@users.noreply.github.com>
2018-09-16 07:09:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,11 @@ def webhook():
 
 def makeWebhookResult(req):  
      if req.get("result").get("action") == "cek":
-        driver = webdriver.Chrome('./chromedriver.exe')
+        chrome_options = Options()
+        chrome_options.binary_location = GOOGLE_CHROME_BIN
+        chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument('--no-sandbox')
+        driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
         
         return {
             "speech": hasil,
