5772c1f0a36d2e1e51c655a811bfb9d249dbe262
snorlak1999 <39644873+snorlak1999@users.noreply.github.com>
2018-09-16 21:11:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,14 @@ def webhook():
 
 def makeWebhookResult(req):  
     if req.get("result").get("action") == "cek":
-        driver = webdriver.Chrome()
+        options = Options()
+        options.add_argument("--headless") # Runs Chrome in headless mode.
+        options.add_argument('--no-sandbox') # Bypass OS security model
+        options.add_argument('--disable-gpu')  # applicable to windows os only
+        options.add_argument('start-maximized') # 
+        options.add_argument('disable-infobars')
+        options.add_argument("--disable-extensions")
+        driver = webdriver.Chrome(chrome_options=options)
         return {
             "speech": "hasi",
             "displayText": "hasi",
