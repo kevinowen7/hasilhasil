91c8741f5012590136a571430cdbdc3f3a5db69a
snorlak1999 <39644873+snorlak1999@users.noreply.github.com>
2018-09-18 19:12:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -39,14 +39,6 @@ def webhook():
 def makeWebhookResult(req):  
     if req.get("result").get("action") == "cek":
         driver = webdriver.PhantomJS()
-        return {
-            "speech": "a",
-            "displayText": "a",
-            #"data": {},
-            #"contextOut": [],
-            "source": "a"
-        }
-        
         driver.get('https://akademik.ithb.ac.id/default.php?mod=roster%20ruangan')
         #driver.find_element_by_link_text("Absensi Kuliah").click();
         #driver.find_element_by_id("txtUsername").send_keys("1")
