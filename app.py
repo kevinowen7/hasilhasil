ef9d58fa4fe3130f516732c9275f9c84815cb73f
snorlak1999 <39644873+snorlak1999@users.noreply.github.com>
2018-09-18 21:00:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -43,6 +43,7 @@ def makeWebhookResult(req):
         chrome_options.add_argument('--disable-gpu')
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--headless')
+        chrome_options.Proxy = null;
         driver = webdriver.Chrome(executable_path="/app/.chromedriver/bin/chromedriver", chrome_options=chrome_options)
         driver.get('https://akademik.ithb.ac.id/default.php?mod=roster%20ruangan')
         #driver.find_element_by_link_text("Absensi Kuliah").click();
