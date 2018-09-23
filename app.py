99dfecb3a7f550edd9c6bcdf4f0b9dc503a95aae
snorlak1999 <39644873+snorlak1999@users.noreply.github.com>
2018-09-23 13:16:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -19,10 +19,10 @@ from flask import make_response
 # Flask app should start in global layout
 app = Flask(__name__)
 
-@app.route('/webhook1', methods=['POST'])
+@app.route('/webhook', methods=['POST'])
 
 
-def webhook1():
+def webhook():
     req = request.get_json(silent=True, force=True)
     res = makeWebhookResult(req)  
     
