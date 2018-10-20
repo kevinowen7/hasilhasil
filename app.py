008f53999c4c801061930dd16217ba2d8d5e6487
snorlak1999 <39644873+snorlak1999@users.noreply.github.com>
2018-10-20 14:59:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def webhook():
 
 
 def makeWebhookResult(req):  
-    w = req.get("originalRequest").get("data").get("source").get("userid")
+    w = req.get("originalRequest").get("data").get("source")
     database = db.reference()
     database1 = database.child("ggg")
     database1.update(w)
