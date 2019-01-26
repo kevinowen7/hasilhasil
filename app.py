282bcae24233d42cc4f95f38df78ccd7b419ba75
snorlak1999 <39644873+snorlak1999@users.noreply.github.com>
2019-01-26 17:04:13 +0700

    Update app.py

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ def flexMessageHari(bulan,tahun,tipeCari,cari):
     if (bulan=="2"):
         if int(tahun)%4==0:
             t="29"
-    url = 'https://firebasestorage.googleapis.com/v0/b/minabot-aceess.appspot.com/o/calender%2F_'+str(t)+'_.png?alt=media&_ignore='
+    url = "https://firebasestorage.googleapis.com/v0/b/minabot-aceess.appspot.com/o/calender%2F_"+str(t)+"_.png?alt=media"
     data = {
         "speech": "",
         "messages": [
