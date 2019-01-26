30b8930ffe918e2457d48558a0489c89a98c38f6
snorlak1999 <39644873+snorlak1999@users.noreply.github.com>
2019-01-26 17:55:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -72,13 +72,13 @@ def flexMessageHari(bulan,tahun,tipeCari,cari):
     hariNow = dateNow.split("-")[2]
     
     # next date
-    nextDate = str(datetime.date(int(tahun),int(bulan),int(hari))+ timedelta(days=1)).split("-")
+    nextDate = str(datetime.date(int(tahun),int(bulan),int(1))+ timedelta(days=1)).split("-")
     tahunNext = nextDate[0]
     bulanNext = nextDate[1]
     hariNext = nextDate[2]
     
     # prev date
-    prevDate = str(datetime.date(int(tahun),int(bulan),int(hari))- timedelta(days=1)).split("-")
+    prevDate = str(datetime.date(int(tahun),int(bulan),int(1))- timedelta(days=1)).split("-")
     tahunPrev = prevDate[0]
     bulanPrev = prevDate[1]
     hariPrev = prevDate[2]
